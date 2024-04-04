489e58c06e04a23ec038968e2ec20ce7819eab74
aejmcclelland <aejmcclelland@googlemail.com>
2024-04-04 17:42:08 +0100

    added spinner and property detail  components

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,9 @@
 // Path: app/properties/index.jsx
 import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
-import Footer from '@/components/Footer';
 import HomeProperties from '@/components/HomeProperties';
 
-const HomePage =  () => {
+const HomePage = () => {
 
     return (
         <>
