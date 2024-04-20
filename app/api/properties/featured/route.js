34a9f0a6ac6df3b30cd4eb4a81aa8612ce139b01
aejmcclelland <aejmcclelland@googlemail.com>
2024-04-20 21:14:17 +0100

    added featured properties route and components

---
+++ app/api/properties/featured/route.js
@@ -0,0 +1,15 @@
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+// GET /api/properties/featured
+export const GET = async (request) => {
+	try {
+		await connectDB();
+
+		const properties = await Property.find({ isFeatured: true });
+
+		return Response.json(properties);
+	} catch (error) {
+		console.log(error);
+		return new Response('Something Went Wrong', { status: 500 });
+	}
+};
--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,14 @@ import Hero from '@/components/Hero';
 import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
 import react from 'react';
+import FeaturedProperties from '@/components/FeaturedProperties';
 const HomePage = () => {
 
     return (
         <>
             <Hero />
             <InfoBoxes />
+            <FeaturedProperties />
             <HomeProperties />
         </>
     );
