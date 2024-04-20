4f47359d07c38e6510d9a571df543e3652f29ba9
aejmcclelland <aejmcclelland@googlemail.com>
2024-04-20 21:41:47 +0100

    added requedts.js

--- utils/requests.js
+++ utils/requests.js
@@ -1,22 +1,25 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
 // Fetch all properties
-async function fetchProperties( {showFeatured = false} = {}) {
+async function fetchProperties({ showFeatured = false } = {}) {
 	try {
-		// Handle the case when the domain is not available yet
+		// Handle the case where the domain is not available yet
 		if (!apiDomain) {
 			return [];
 		}
 
-		const res = await fetch(`${apiDomain}/properties${showFeatured ?'/featured' : ''}`, { cache: 'no-store' });
-	
+		const res = await fetch(
+			`${apiDomain}/properties${showFeatured ? '/featured' : ''}`,
+			{ cache: 'no-store' }
+		);
+
 		if (!res.ok) {
 			throw new Error('Failed to fetch data');
 		}
 
-		return await res.json();
+		return res.json();
 	} catch (error) {
-		console.error('Error fetching properties:', error);
+		console.log(error);
 		return [];
 	}
 }
